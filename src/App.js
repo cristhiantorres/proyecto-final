ea84e38aef8869e831bb8bf7a29fadce535803c5
Cristhian Torres <cristhiantorres95@gmail.com>
2018-01-31 17:54:24 -0300

    Se agrega bootstrap

--- src/App.js
+++ src/App.js
@@ -6,27 +6,15 @@ import './App.css';
 
 class App extends Component {
   render() {
-    const {counter} = this.props;
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React + MobX</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Counter:
-          <span className={counter.isOdd ? 'Counter-odd' : 'Counter-even'}> {counter.count} </span>
-        </p>
-        <p>
-          <button onClick={() => counter.increment()}> + </button>
-          <button onClick={() => counter.decrement()}> - </button>
-        </p>
-        <DevTools />
+
+      <div className="container">
+
+        <h2>Hola Mundo</h2>
+
       </div>
-    );
+
+      );
   }
 }
 
